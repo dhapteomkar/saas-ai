606cea945e282c9d93bb03496ac5aff7c5f2dc40
dhapteomkar <117029497+dhapteomkar@users.noreply.github.com>
2024-05-03 03:57:43 +0530

    Update page.tsx

--- app/(dashboard)/(root)/dashboard/page.tsx
+++ app/(dashboard)/(root)/dashboard/page.tsx
@@ -16,12 +16,12 @@ const tools = [
         href: "/conversation",
         color: "text-violet-500",
         bgColor: "bg-violet-500/10",
-        href: "/conversation",
+       
     },
     {
         label: "Music Generation",
         icon: Music,
-        href: "/conversation",
+     
         color: "text-emerald-500",
         bgColor: "bg-violet-500/10",
         href: "/music",
@@ -29,7 +29,7 @@ const tools = [
     {
         label: "Image Generation",
         icon: ImageIcon,
-        href: "/conversation",
+       
         color: "text-pink-700",
         bgColor: "bg-pink-700/10",
         href: "/image",
@@ -38,7 +38,7 @@ const tools = [
     {
         label: "Code Generation",
         icon: Code,
-        href: "/conversation",
+    
         color: "text-green-700",
         bgColor: "bg-green-700/10",
         href: "/code",
